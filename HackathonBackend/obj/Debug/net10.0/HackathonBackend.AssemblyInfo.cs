13491aa69efe144c81f2b829774d17d4637b8e5e
anis.redmond <anis.redmond@gmail.com>
2026-05-02 14:29:41 -0400

    blah blah blah

--- HackathonBackend/obj/Debug/net10.0/HackathonBackend.AssemblyInfo.cs
+++ HackathonBackend/obj/Debug/net10.0/HackathonBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HackathonBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cc75feff1d3941ffa27a9825bc07fd09ba5cc30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f9056d897889d7e34a3ad8d6f002c4a5f3838df")]
 [assembly: System.Reflection.AssemblyProductAttribute("HackathonBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HackathonBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
