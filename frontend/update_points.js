8151c2f6d10bf47a0f0792515f94471a81430e9f
anis.redmond <anis.redmond@gmail.com>
2026-05-02 15:10:26 -0400

    addded update_piints.js

--- frontend/update_points.js
+++ frontend/update_points.js
@@ -2,6 +2,23 @@
 
 function updatePoints() 
 {
-    
+    fetch("http://10.230.122.4:5001/api/usercontrollers/UpdateUserPoints", {//send signup info to db
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            user_id: localStorage.getItem("user_id"),
+            points: 10
+        })
+    })
+    .then(data => {
+        localStorage.setItem("points", JSON.stringify(data));
+        console.log(data);
+    })
+    .catch(error => {
+        console.error(error);
+        alert(error.message);
+    });
 
 }
