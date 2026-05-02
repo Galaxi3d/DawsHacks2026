5555e5b1ea3aa32385443b87d469b0ce18d9e4de
peytonmandato <peyton.mandato@gmail.com>
2026-05-02 12:56:56 -0400

    personal task list complete - ready for CSS

---
+++ script.js
@@ -0,0 +1,33 @@
+// https://www.youtube.com/watch?v=G0jO8kUrg-I
+
+const blankBox = document.getElementById("blank_box");
+const listContainer = document.getElementById("list_container");
+function AddTask(){
+    if(blankBox.value===''){
+        alert("There is no task to check off!")
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=blankBox.value;
+        listContainer.appendChild(li);
+        let 
+    }
+    blankBox.value = '';
+    saveData()
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked"); 
+        saveData()
+    }
+});
+// This saves the list item as data in the browser's storage (I think)
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+// This shows the data when you go pack to the site
+function showData(){
+    listContainer.innerHTML = localStorage.getItem("data")
+}
+showData()
