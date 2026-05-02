0741281ae6cb213b8144859c956ee7f142815681
Sofia <sachalouro@icloud.com>
2026-05-02 14:51:36 -0400

    working on login

---
+++ frontend/login.js
@@ -0,0 +1,4 @@
+function loginValues(){
+    const email=document.getElementById("email").value.trim();
+    const passwd=document.getElementById("passwd").value;
+}
--- frontend/signup.js
+++ frontend/signup.js
@@ -1,19 +1,19 @@
-function formValue(){
+function signupValues(){
     // gather user input to be put into db
-    const firstName = document.getElementById("firstName").value.trim();
-    const lastName = document.getElementById("lastName").value.trim();
-    const email = document.getElementById("email").value.trim();
-    const passwd = document.getElementById("passwd").value;
+    const firstName=document.getElementById("firstName").value.trim();
+    const lastName=document.getElementById("lastName").value.trim();
+    const email=document.getElementById("email").value.trim();
+    const passwd=document.getElementById("passwd").value;
 
-    if (firstName && lastName && email && passwd.length >= 5) {
+    if (firstName && lastName && email && passwd.length >= 5) {//input validation. all values must hava data, password longer than 5 char
         signupUpload(firstName, lastName, email, passwd);
     } else {
-        alert("Please fill in all fields and use a password with at least 5 characters.");
+        alert("Please fill in all fields and use a password with at least 5 characters.");//incorrect input message
     }
 }
 
 function signupUpload(firstName, lastName, email, passwd){
-    fetch("http://10.230.122.4:5001/api/usercontrollers/RegisterUser", {
+    fetch("http://10.230.122.4:5001/api/usercontrollers/RegisterUser", {//send signup info to db
         method: "POST",
         headers: {
             "Content-Type": "application/json"
